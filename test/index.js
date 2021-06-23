4797382b137fe30798a1e63ad82b112c07c10f7b
giacomomanzo <86232551+giacomomanzo@users.noreply.github.com>
2021-06-23 08:35:38 +0200

    Add files via upload

---
+++ test/index.js
@@ -0,0 +1,10 @@
+$.ajax({
+    url:'https://assets.fc-dev.instore.oakley.com/assets/products/products.json',
+    cache:false
+}).done(function(data){
+    console.log(data);
+    
+    
+    
+    
+});
